4278a1a8dae745434e348f8c63b92b4addbc5486
Maryna-Orel <103521798+Maryna-Orel@users.noreply.github.com>
2022-10-11 13:28:22 +0300

    HW 10 fixed2

--- src/refs.js
+++ src/refs.js
@@ -1,5 +1,5 @@
 export const refs = {
-    inputSearch: document.querySelector('#search-box'),
+inputSearch: document.querySelector('#search-box'),
 countryList: document.querySelector('.country-list'),
 countryInfo: document.querySelector('.country-info'),
 }
